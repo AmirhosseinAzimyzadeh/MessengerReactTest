f80d56aaea9112ac1235484163b6c36fe2a59e74
amirhossein <amirhosseinazimyzadeh2@gmail.com>
2020-01-01 20:12:13 +0330

    handle message Enter from user

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import '../componentStyle/textbox.css'
-class TextBox extends Component{
-    constructor(){
+class TextBox extends Component {
+    constructor() {
         super()
+        this.handlePressEnter = this.handlePressEnter.bind(this);
+    }
+    //handle message from user
+    handlePressEnter(event) {
+        let message;
+        if(event.key === 'Enter'){
+            console.log('Enter pressed')
+            message = event.target.value;
+            console.log(message);
+            event.target.value = ""
+        }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div id="tHolder">
-                <input placeholder="Type your message ..." autoFocus id="textBox" />
+                <input 
+                onKeyPress={this.handlePressEnter} placeholder="Type your message ..." autoFocus id="textBox" />
             </div>
         );
     }
